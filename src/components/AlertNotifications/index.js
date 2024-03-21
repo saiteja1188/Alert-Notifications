b2ff1e9db315ddf82b7bfcdf801fa5b61e0735a8
saiteja1188 <loveui19@gmail.com>
2024-03-21 16:35:27 +0000

    cl Alert Notificatin

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,55 +1,54 @@
-// Write your code here
 import {AiFillCheckCircle} from 'react-icons/ai'
 import {RiErrorWarningFill} from 'react-icons/ri'
-import {MdWarning} from 'react-icons/md'
-import {MdInfo} from 'react-icons/md'
+import {MdWarning, MdInfo} from 'react-icons/md'
 
 import Notification from '../Notification'
+
 import './index.css'
 
 const AlertNotifications = () => {
-  const renderInfoView = () => (
+  const renderInfoNotification = () => (
     <Notification>
       <MdInfo className="info icon" />
-      <div className="success-container">
-        <h1 className="content-heading info">Info</h1>
-        <p className="content-description">
+      <div className="message-container">
+        <h1 className="message-heading info">Info</h1>
+        <p className="description">
           Anyone on the internet can view these files
         </p>
       </div>
     </Notification>
   )
 
-  const renderWarningView = () => (
+  const renderWarningNotification = () => (
     <Notification>
       <MdWarning className="warning icon" />
-      <div className="success-container">
-        <h1 className="content-heading warning">Warning</h1>
-        <p className="content-description">
+      <div className="message-container">
+        <h1 className="message-heading warning">Warning</h1>
+        <p className="description">
           Viewers of this file can see comments and suggestions
         </p>
       </div>
     </Notification>
   )
 
-  const renderErrorView = () => (
+  const renderErrorNotification = () => (
     <Notification>
       <RiErrorWarningFill className="error icon" />
-      <div className="container">
-        <h1 className="content-heading error">Error</h1>
-        <p className="content-description">
+      <div className="message-container">
+        <h1 className=" message-heading error">Error</h1>
+        <p className="description">
           Sorry, you are not authorized to have access to delete the file
         </p>
       </div>
     </Notification>
   )
 
-  const renderSuccessView = () => (
+  const renderSuccessNotification = () => (
     <Notification>
       <AiFillCheckCircle className="success icon" />
-      <div className="success-container">
-        <h1 className="content-heading success">Success</h1>
-        <p className="content-description">
+      <div className="message-container">
+        <h1 className="message-heading success">Success</h1>
+        <p className="description">
           You can access all the files in the folder
         </p>
       </div>
@@ -58,12 +57,12 @@ const AlertNotifications = () => {
 
   return (
     <div className="app-container">
-      <div className="alert-notification-content">
+      <div className="responsive-container">
         <h1 className="heading">Alert Notifications</h1>
-        {renderSuccessView()}
-        {renderErrorView()}
-        {renderWarningView()}
-        {renderInfoView()}
+        {renderSuccessNotification()}
+        {renderErrorNotification()}
+        {renderWarningNotification()}
+        {renderInfoNotification()}
       </div>
     </div>
   )
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,15 +1,14 @@
-// Write your code here
 import {GrFormClose} from 'react-icons/gr'
 
 import './index.css'
 
 const Notification = props => {
   const {children} = props
-  console.log(children)
+
   return (
     <div className="notification">
       <div className="notification-content">{children}</div>
-      <GrFormClose className="c-icon" />
+      <GrFormClose className="close-icon" />
     </div>
   )
 }
